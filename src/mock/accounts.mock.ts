5b8c9b2d107fa8cfec6a80f68f26a4b433b55805
Igor Rybyakov <rybyakovigor@gmail.com>
2022-07-03 14:21:12 +0500

    chore: remove mock from gitignore

---
+++ src/mock/accounts.mock.ts
@@ -0,0 +1,27 @@
+import { Account } from '../types-space/types/account.type';
+
+const accounts: Account[] = [
+  {
+    id: '1',
+    title: 'Google',
+    login: 'user1',
+    password: '123456',
+    url: 'google.com',
+  },
+  {
+    id: '2',
+    title: 'Yandex',
+    login: 'user2',
+    password: '123456',
+    url: 'yandex.com',
+  },
+  {
+    id: '3',
+    title: 'Dodle',
+    login: 'user3',
+    password: '123456',
+    url: 'dodle.com',
+  },
+];
+
+export default accounts;
